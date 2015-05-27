6b163e81d7e852fd71f5b24b46b81ed61fa519b4
Mateus <mmachado.csp@gmail.com>
2015-05-26 22:21:06 -0300

    .

--- src/br/com/webservice/Consumer.java
+++ src/br/com/webservice/Consumer.java
@@ -1,5 +1,13 @@
 package br.com.webservice;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
+
 public class Consumer {
 	public static void main(String[] args) {
 		// Service
@@ -7,6 +15,14 @@ public class Consumer {
 
 		// Proxy
 		Random proxy = randomService.getRandomPort();
+
+		// Username e Password
+		BindingProvider bp = (BindingProvider) proxy;
+		Map<String, List<String>> headers = new HashMap<String, List<String>>();
+		headers.put("Username", Collections.singletonList("k19"));
+		headers.put("Password", Collections.singletonList("k23"));
+		bp.getRequestContext()
+				.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
 
 		// Operation
 		double next = proxy.next(50);
